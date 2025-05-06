cbb83e99f024734564b4cffc4f6774c09592e2b2
saigaddam1320 <162509739+saikrishna091@users.noreply.github.com>
2025-05-06 12:34:11 -0500

    Update week_1.py

--- week_1.py
+++ week_1.py
@@ -3,4 +3,4 @@ sex = "male"
 age = 25
 height = 150
 print(
-    f"My name is {name}, I am {sex} ,I am {age} years old, and my height is {height} meters.")
+    f"My name is {name}, I am {sex} ,I am {age} years old, and my height is {height} cm.")
