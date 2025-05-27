e5792f171938cb768df37bceea0c13ced54e1394
saigaddam1320 <162509739+saikrishna091@users.noreply.github.com>
2025-05-27 16:03:48 -0500

    Create Week_4.py

---
+++ Week_4.py
@@ -0,0 +1,13 @@
+# using the for loop
+for number in range(1, 51):
+    if number % 2 == 0:
+        print(number)
+
+
+# using the while loop
+
+number = 1
+while number <= 50:
+    if number % 2 == 0:
+        print(number)
+    number += 1
