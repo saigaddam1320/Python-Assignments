2b6541839a594fc5a1991d97f1c2baee3a9dd575
saigaddam1320 <162509739+saikrishna091@users.noreply.github.com>
2025-06-18 14:41:22 -0500

    Create Week_7.py

---
+++ Week_7.py
@@ -0,0 +1,8 @@
+
+my_tuple = (1, 2, 2, 3, 4, 5, 5, 6, 7, 8, 8, 9)
+
+my_set = set(my_tuple)
+
+# Step 3: Print the result
+print("Original Tuple:", my_tuple)
+print("Converted Set (duplicates removed):", my_set)
