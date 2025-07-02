6e89b1a0c98048b627a7e8a65ffc1e9836df9629
saigaddam1320 <162509739+saikrishna091@users.noreply.github.com>
2025-07-02 12:56:19 -0500

    Create Week_9.py

---
+++ Week_9.py
@@ -0,0 +1,8 @@
+def square(num):
+    return num * num
+
+
+# Test the function with three inputs
+print("Square of 8 is:", square(8))
+print("Square of -5 is:", square(-5))
+print("Square of 0 is:", square(0))
