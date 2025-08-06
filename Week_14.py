cbdecb628f28221e1d6bd0c2adf10cbba955ff81
saigaddam1320 <162509739+saikrishna091@users.noreply.github.com>
2025-08-06 11:54:26 -0500

    Create Week_14.py

---
+++ Week_14.py
@@ -0,0 +1,14 @@
+
+file_name = "example.txt"
+
+
+with open(file_name, "a") as f:
+    f.write("is this the new line.\n")
+
+
+with open(file_name, "r") as f:
+    lines = f.readlines()
+
+last_three = lines[-3:]
+for line in last_three:
+    print(line.strip())
